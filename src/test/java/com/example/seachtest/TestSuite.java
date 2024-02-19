35d3572ca4fbd29a1c74a4d5d65b38284879fa3e
Barkatina <159892946+Barkatina@users.noreply.github.com>
2024-02-19 10:56:14 +0300

    Initial commit

---
+++ src/test/java/com/example/seachtest/TestSuite.java
@@ -0,0 +1,11 @@
+package com.example.seachtest;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.example.seachtest")
+
+
+public class TestSuite {
+}
